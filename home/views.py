ea8c3132dcadbf85b859269bb64b10630d4a5603
MrHacibata <rd5005.3619@gmail.com>
2025-06-08 21:41:56 +0200

    set up home.html
    set up css and content for home.html
    created include-header

--- home/views.py
+++ home/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "home/index.html")
+    return render(request, "home/index.html",{
+        'films_range': range(5),
+        'rows_range': range(5)
+    })
