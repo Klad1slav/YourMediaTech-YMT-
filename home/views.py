3635d3f660c5d4f977340c3bc07310a821912227
Vladyslav Maistrenko <153518193+Klad1slav@users.noreply.github.com>
2025-06-22 19:24:12 +0200

    TMDB(test API)

--- home/views.py
+++ home/views.py
@@ -1,9 +1,21 @@
+import requests
 from django.shortcuts import render
+from django.conf import settings
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home/index.html",{
-        'films_range': range(5),
-        'rows_range': range(5)
+    API_KEY = settings.TMDB_API_KEY
+    url = f'https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&language=en-US&page=1'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        movies = data.get('results', []) [:30]
+    else:
+        movies = []
+
+    return render(request, "home/index.html", {'movies': movies
     })
+
