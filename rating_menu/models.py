83da85a5f3b8b4c8e940fe2a67b7ff3fdc4cafc5
Vladyslav Maistrenko <153518193+Klad1slav@users.noreply.github.com>
2025-07-10 10:19:45 +0200

    Media_item(first)

--- rating_menu/models.py
+++ rating_menu/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 class MediaItem(models.Model):
-    search_field = models.CharField(max_length=100)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='media_items', default=None, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)  # Assuming rating is an integer
+    poster_url = models.URLField(default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0OCD8vwRqyldCWESsUQ9_r2YQzHMPHXi1MQ&s")  # Default poster URL
+    tmdb_id = models.IntegerField(default=0)  # Assuming tmdb_id is an integer
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} {self.description} ({self.rating}/10) {self.created_at.strftime('%Y-%m-%d %H:%M:%S') if self.created_at else 'N/A'}" 
