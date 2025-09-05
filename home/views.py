b211caf07701be5f629b26af9f02fc9e2fcf5bd3
Vladyslav Maistrenko <153518193+Klad1slav@users.noreply.github.com>
2025-09-05 22:24:50 +0200

    games_recommendations(first try)

--- home/views.py
+++ home/views.py
@@ -8,12 +8,13 @@ from django.shortcuts import redirect
 
 def get_tmdb_recommendations(slug):
     API_KEY = settings.TMDB_API_KEY
+    RAWG_API_KEY = settings.RAWG_API_KEY
     urls = {
         "films": f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=en-US&page=1",
         "series": f"https://api.themoviedb.org/3/tv/top_rated?api_key={API_KEY}&language=en-US&page=1",
         "anime": f"https://api.themoviedb.org/3/tv/top_rated?api_key={API_KEY}&language=en-US&page=1",
         "toons": f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=en-US&page=1",
-        "games": "",
+        "games": f"https://api.rawg.io/api/games?key={RAWG_API_KEY}&ordering=-rating&page_size=20",
         "books": "",
     }
 
@@ -24,17 +25,44 @@ def get_tmdb_recommendations(slug):
     response = requests.get(url)
     if response.status_code != 200:
         return []
+    
 
     data = response.json()
-    movie_list = data.get("results", [])[:20]
+    items_list = data.get("results", [])[:20]
 
-    # Format for tv/anime
-    if slug in ["series", "anime"]:
-        for item in movie_list:
+    # For TMDB media types
+    if slug in ["films", "toons"]:
+        for item in items_list:
+            item["title"] = item.get("title")
+            item["release_date"] = item.get("release_date")
+            item["overview"] = item.get("overview", "")
+            poster_path = item.get("poster_path")
+            if poster_path:
+                item["poster_url"] = f"https://image.tmdb.org/t/p/w500{poster_path}"
+            else:
+                item["poster_url"] = ""
+    elif slug in ["series", "anime"]:
+        for item in items_list:
             item["title"] = item.get("name")
             item["release_date"] = item.get("first_air_date")
+            item["overview"] = item.get("overview", "")
+            poster_path = item.get("poster_path")
+            if poster_path:
+                item["poster_url"] = f"https://image.tmdb.org/t/p/w500{poster_path}"
+            else:
+                item["poster_url"] = ""
+    elif slug == "games":
+        for item in items_list:
+            item["title"] = item.get("name")
+            item["release_date"] = item.get("released")
+            item["overview"] = item.get("description_raw", "")
+            item["poster_url"] = item.get("background_image", "")
+    else:
+        # For other types like books or unknown, return empty list
+        return []
 
-    return movie_list
+    return items_list
+
 
 
 def home(request, slug="films"):
@@ -46,10 +74,10 @@ def home(request, slug="films"):
             slug = request.POST.get("media_type")
             return redirect('home', slug=slug)
     
-    movie_list = get_tmdb_recommendations(slug)
+    items_list = get_tmdb_recommendations(slug)
     
     # PAGINATOR: Show 20 movies per page
-    paginator = Paginator(movie_list, 12)  
+    paginator = Paginator(items_list, 12)  
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -59,4 +87,3 @@ def home(request, slug="films"):
                     'slug': slug,
                      
     })
-
