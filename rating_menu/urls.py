04713b3e39743acc63dc098af6600cf2f84e76d8
MrHacibata <rd5005.3619@gmail.com>
2025-09-16 20:22:38 +0200

    added book section, optimised code

--- rating_menu/urls.py
+++ rating_menu/urls.py
@@ -7,3 +7,10 @@ urlpatterns = [
     path("<slug:slug>", views.index, name="rating_menu"),
     # path("", views)
 ]
+
+# from .views_class import IndexView
+
+# urlpatterns = [
+#     path('rating_menu/<slug:slug>/', IndexView.as_view(), name='rating_menu'),
+    
+# ]
